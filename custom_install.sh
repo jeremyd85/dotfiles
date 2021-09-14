0352af88d0426f7c987915d778ad17cb101b6e90
jeremyd85 <jeremydevore85@gmail.com>
2021-09-14 10:16:01 -0500

    Added files to list packages to be installed

---
+++ custom_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install Intelij IDEA (Toolbox)
+# https://www.jetbrains.com/toolbox-app/
+
+# Wine:
+# https://wiki.winehq.org/Ubuntu
+
+# Lutris:
+# https://lutris.net/downloads/
+
+# GNS3:
+# https://docs.gns3.com/docs/getting-started/installation/linux/
+
